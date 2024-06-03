1ed5d3466f9597ec5538ae3eb1ce4417cae90129
ashiph-ui <76753545+ashiph-ui@users.noreply.github.com>
2024-06-03 14:04:26 +0100

    camera can detect landmarks points on hand

--- camera.py
+++ camera.py
@@ -4,6 +4,12 @@ import mediapipe as mp
 from tensorflow import keras
 
 cap = cv.VideoCapture(0)
+
+# hand detection
+mpHands = mp.solutions.hands
+mpHandsMesh = mpHands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.75, min_tracking_confidence=0.5)
+mpDraw = mp.solutions.drawing_utils
+
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
@@ -11,14 +17,22 @@ if not cap.isOpened():
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+    output = mpHandsMesh.process(rgb)
+    
+    if output.multi_hand_landmarks:
+        for i in output.multi_hand_landmarks:
+            mpDraw.draw_landmarks(frame, i, mpHands.HAND_CONNECTIONS,
+            landmark_drawing_spec=mpDraw.DrawingSpec(color=(0,255,0), thickness=2, circle_radius=5))
+    
+    
  
     # Display the resulting frame
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
+    cv.imshow('frame', frame)   
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
